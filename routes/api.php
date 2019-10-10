3245ea86c7f2ce14df432c19f08242bffd772f20
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2019-10-10 20:48:59 +0200

    WIP

--- routes/api.php
+++ routes/api.php
@@ -23,13 +23,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('/v1/channels', [ChannelsController::class, 'index' ] );
 Route::get('/v1/channels/{channel}', [ChannelsController::class, 'show' ] );
 
-Route::group(['middleware' => ['auth']], function () {
+//Route::group(['middleware' => ['auth']], function () {
     Route::post('/v1/channels', [ChannelsController::class, 'store' ] );
 //    Route::delete('/v1/channels/{id}', [ChannelsController::class, 'destroy' ] );
     // ROUTE MODEL BINDING
     Route::delete('/v1/channels/{channel}', [ChannelsController::class, 'destroy' ] );
     Route::put('/v1/channels/{channel}', [ChannelsController::class, 'update' ] );
-});
+//});
 
 Route::get('/v1/messages', [MessagesController::class, 'index' ] );
 Route::get('/v1/messages/{id}', [MessagesController::class, 'index' ] );
